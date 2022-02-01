275e1772ef13a16f10ca0f6368f4005430e8844f
csm.rd <chetanmandge05@gmail.com>
2022-02-01 10:35:14 +0530

    add student class and create object for same

--- oops.py
+++ oops.py
@@ -1,1 +1,19 @@
+print('hello')
 
+
+class Students():
+    def __init__(self, name, roll, marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+
+    def display(self):
+        print(f"student name is {self.name} marks is {self.marks} and roll number is {self.roll}")
+
+
+s1 = Students('chetan',3,56)
+s1.display()
+
+
+s2 = Students('nilu',34,99)
+s2.display()
