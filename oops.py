b2ed0fd1234d5416a677db3d5e546256a183dc13
csm.rd <chetanmandge05@gmail.com>
2022-02-03 11:12:10 +0530

    update comments

--- oops.py
+++ oops.py
@@ -1,21 +1,23 @@
-
 class Students:
-    company_nam e ='Flash electronics'   # this is class/ static variable
-    def __init__(self, name, roll, marks): # in init function the number of veriable you have to pass this is called local variable
-        self.name = name # self.name,self.roll,self.marks is instance variable
+    company_name = 'Flash electronics'  # this is class/ static variable
+
+    def __init__(self, name, roll, marks):
+        # in init function the number of variable you have to pass this is called local variable
+        self.name = name  # self.name,self.roll,self.marks is instance variable
         self.roll = roll
         self.marks = marks
+
     @classmethod  # class method access by class name only
     def display(cls):
         print(f"company name is {cls.company_name} ")
 
     @staticmethod
-    def add_num(num1 ,num2):
-        print(f"addition of {num1} and {num2} is {num2 +num1} ")
+    def add_num(num1, num2):
+        print(f"addition of {num1} and {num2} is {num2 + num1} ")
 
 
-s1 = Students('chetan' ,3 ,56)
+s1 = Students('chetan', 3, 56)
 Students.display()
-Students.add_num(1 ,8)
+Students.add_num(1, 8)
 
-s2 = Students('nilu' ,34 ,99)
+s2 = Students('nilu', 34, 99)
