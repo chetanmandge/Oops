2e59e01e0b8c2684cd679abfba4302ac308a6a6e
csm.rd <chetanmandge05@gmail.com>
2022-02-03 10:19:27 +0530

    add staticmethod and class method

--- oops.py
+++ oops.py
@@ -1,20 +1,21 @@
-print('hello')
-
 
 class Students:
-    company_name='Flash electronics'
-    def __init__(self, name, roll, marks):
-        self.name = name
+    company_nam e ='Flash electronics'   # this is class/ static variable
+    def __init__(self, name, roll, marks): # in init function the number of veriable you have to pass this is called local variable
+        self.name = name # self.name,self.roll,self.marks is instance variable
         self.roll = roll
         self.marks = marks
+    @classmethod  # class method access by class name only
+    def display(cls):
+        print(f"company name is {cls.company_name} ")
 
-    def display(self):
-        print(f"student name is {self.name} marks is {self.marks} and roll number is {self.roll}")
+    @staticmethod
+    def add_num(num1 ,num2):
+        print(f"addition of {num1} and {num2} is {num2 +num1} ")
 
 
-s1 = Students('chetan',3,56)
-s1.display()
-print(Students.company_name)
+s1 = Students('chetan' ,3 ,56)
+Students.display()
+Students.add_num(1 ,8)
 
-s2 = Students('nilu',34,99)
-s2.display()
+s2 = Students('nilu' ,34 ,99)
