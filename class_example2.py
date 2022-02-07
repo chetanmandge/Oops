330039b58973dde827cb19bc68b8b1d8c66f503e
csm.rd <chetanmandge05@gmail.com>
2022-02-07 15:15:36 +0530

    dog class for add or upend data in list and make special list for each dog

--- class_example2.py
+++ class_example2.py
@@ -1,12 +1,16 @@
 class dog():
-    tricks=[]
-    def __init__(self,name):
-        self.name=name
-    def add_tricks(self,trick):
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+
+    def add_tricks(self, trick):
         self.tricks.append(trick)
 
-d=dog('fido')
-e=dog('buddy')
+
+d = dog('fido')
+e = dog('buddy')
 d.add_tricks('roll over')
 e.add_tricks('play dead')
-print(d.tricks,d.name)
+print(d.tricks, d.name)
+print(e.tricks, e.name)
