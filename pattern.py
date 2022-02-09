ee4965c4a2f5edba661d4a3c0010e901bff1141d
csm.rd <chetanmandge05@gmail.com>
2022-02-09 13:06:40 +0530

    pattern program

--- pattern.py
+++ pattern.py
@@ -1,5 +1,37 @@
-
-for i in range(1, 6):
+''' forword pattern
+*
+**
+***
+****
+*****'''
+for i in range(1, 5):
     for j in range(i):
         print("*", end='')
     print(" ")
+''' reverse pattern 
+*****
+****
+***
+**
+*
+'''
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end='')
+    print()
+# i=0
+# while(i<=5):
+#     for j in range(1,i+1):
+#         print("*",end='')
+#     i+=1
+#     print()
+'''If combine both pattern we get below pattern 
+* 
+** 
+*** 
+**** 
+*****
+****
+***
+**
+*'''
