4b75daa6f4adf919302e2ef5b331bf0dd59736b2
csm.rd <chetanmandge05@gmail.com>
2022-02-11 15:47:33 +0530

    add different methods

--- Employee_managment_opps.py
+++ Employee_managment_opps.py
@@ -7,10 +7,19 @@ class employee_managment():
         self.id = id
         self.dept = dept
 
+    def Add_employe(self):
+        u_name = input("Enter employee Name :-")
+        u_id = input('Enter employee ID :-')
+        u_dept = input("Enter employee department :-")
+
+        e2 = employee_managment(u_name, u_id, u_dept)
+        dB.append(e2)
+        print(f"Employee {u_name} added successfully")
+
 
 e1 = employee_managment("chetan", 201, 'etc')
 dB.append(e1)
-
+obj = employee_managment("", 0, "")
 while 1:
     print("             Welcome to employee management system ")
     print('''   
@@ -24,14 +33,25 @@ while 1:
             ''')
     ch = int(input("Enter your choice for employee :-"))
     if ch == 1:
-        pass
+        obj.Add_employe()
+
     elif ch == 2:
+
         pass
     elif ch == 3:
-        pass
+        for i in range(len(dB)):
+            print("Employee name is :-", dB[i].name)
+            print("Employee id is :-", dB[i].id)
+            print("Employee dept is :-", dB[i].dept)
+
     elif ch == 4:
         pass
-    elif ch== 5:
+    elif ch == 5:
+        nam = input("enter employee name you want to find :-")
+        for i in range(len(dB)):
+            if dB[i].name == nam:
+                print("found employee in Database")
+
         pass
     elif ch == 6:
         break
@@ -40,5 +60,5 @@ while 1:
 
     choice = input("Do you want to continue Y/N :-")
     choice.lower()
-    if(choice == 'n'):
+    if (choice == 'n'):
         break
